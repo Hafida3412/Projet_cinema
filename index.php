ca19fd9dbe5981f37a58c94050de2f24d95e98c7
Hafida 3412 <lounsy81@gmail.com>
2024-04-17 09:00:57 +0200

    projet cinema debut index.php et connect.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Controller\CinemaController;
+
+spl_autoload_register(function($class_name){
+    include $class_name . '.php';
+});
+
+$ctrlCinema = new CinemaController();
+
+if(isset($_GET["action"])){
+    switch ($_GET["action"]){
+
+        case "listFilms" : $ctrlCinema->listFilms(); break;
+        case "listActeurs" : $ctrlCinema->listActeurs(); break;
+    }
+}
---
+++ model/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Model;
+
+abstract class Connect{
+
+    const HOST = "localhost";
+    const DB = "cinema";
+    const USER = "root";
+    const PASS = "";
+
+    public static function se Connecter(){
+        try{
+            return new \PDO(
+                "mysql:host=".self::HOST.";dbname=".self::DB."; charset=utf8", self::USER, self::PASS);
+            } catch(\PDOException $ex){
+                return $ex->getMessage();
+            }
+    }
+}
