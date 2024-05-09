89e2339c97f2a8a6715bfc244add74e8d5d40114
Hafida 3412 <lounsy81@gmail.com>
2024-05-09 16:39:38 +0200

    listGenres modifié

--- view/listGenres.php
+++ view/listGenres.php
@@ -7,16 +7,21 @@ if(isset($_SESSION['message'])) {
     unset($_SESSION['message']); // Effacer le message pour ne pas l'afficher à nouveau
 }
 ob_start(); ?><!--pour commencer la vue-->
+
+<!--FORMULAIRE POUR RAJOUTER DES GENRES-->
 <form action="index.php?action=ajouterGenre" method="post"> 
     <input type="hidden" name="action" value="ajouterGenre">
      <label for="nom_genre">Nom du genre:</label> 
      <input type="text" id="nom_genre" name="nom_genre"> 
      <button type="submit" name="submit">Ajouter le genre</button> </form>
 
-
-
+<!--FILTRE POUR VERIFIER LES ENTREES DANS LE FORMULAIRE-->
+<?php
+$nom_genre = filter_input(INPUT_POST, "nom_genre", FILTER_SANITIZE_FULL_SPECIAL_CHARS);    
+?>
 <p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount() ?> genres cinématographiques</p>
 
+<!--AFFICHER UN MESSAGE DE SUCCES-->
 <?php if($message !== "") { ?>
     <div class="uk-alert-success" uk-alert>
         <a class="uk-alert-close" uk-close></a>
