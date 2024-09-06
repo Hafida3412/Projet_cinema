ce9561ca1f8352ddc6b7f646653c5c499526ff34
Hafida3412 <lounsy81@gmail.com>
2024-09-06 11:23:38 +0200

    rajout de commentaires à la vue listActeurs

--- view/listActeurs.php
+++ view/listActeurs.php
@@ -1,9 +1,12 @@
 <?php 
 
+//Démarrage de la temporisation de sortie
 ob_start(); ?><!--pour commencer la vue-->
 
-
-<p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount() ?> acteurs</p>
+<!-- Début de la section HTML pour afficher les données -->
+<p class="uk-label uk-label-warning">
+    <!-- Affichage du nombre d'acteurs trouvés dans la requête -->
+    Il y a <?= $requete->rowCount() ?> acteurs</p>
 
 
 <table class="uk-table uk-table-striped">
@@ -16,7 +19,10 @@ ob_start(); ?><!--pour commencer la vue-->
     <tbody>
     <?php foreach($requete->fetchAll() as $personne) { ?>
             <tr>
-                <td><a href="index.php?action=acteur&id=<?= $personne['id_personne'] ?>"><?= $personne['nom'] ?></a></td>
+                <!-- Lien vers la page de l'acteur avec son ID et affichage du nom -->
+                <td><a href="index.php?action=acteur&id=<?= 
+                 $personne['id_personne'] ?>"><?= $personne['nom'] ?></a></td>
+                 <!-- Affichage du prénom de l'acteur -->
                 <td><?= $personne["prenom"] ?></td>
             </tr>
         <?php } ?>
@@ -29,8 +35,11 @@ echo "<style>";
 echo "th, td { border: 1px solid black; text-align: left; }";
 echo "</style>";
 
+// Définition des variables pour le titre
 $titre = "Liste des acteurs";
 $titre_secondaire = "Liste des acteurs";
+
+// Fin de la temporisation de sortie et stockage du contenu dans $contenu
 $contenu = ob_get_clean();/*"ob_get_clean()" pour terminer la vue
 tout ce qui se trouve entre ces 2 fonctions(temporisation de sortie)
 pour stocker le contenu dans une variable $contenu*/
